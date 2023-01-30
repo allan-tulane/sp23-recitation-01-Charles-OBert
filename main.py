a911e6e2ec4be4c3c3a1ec2a386c88cb20337c3f
Charles O'Bert <123272331+Charles-OBert@users.noreply.github.com>
2023-01-30 20:04:26 +0000

    Finally completed the compare_search function

--- main.py
+++ main.py
@@ -108,21 +108,18 @@ def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
 			return(lst)
 
 	array_list =[]
-	for i in array_list:
+	for i in range(len(sizes)):
 		array_list.append(createList(sizes[i]))
 	blist_times =[]
-	for i in blist_times:
-		blist_times.append( time_search(binary_search(array_list[i]),-1))
+	for i in range(len(sizes)):
+		blist_times.append(time_search(binary_search,array_list[i],-1))
 	linlist_times =[]
 	print("Blist times: ",blist_times)
-	for i in linlist_times:
-		linlist_times.append( time_search(linear_search(array_list[i]),-1))
+	for i in range(len(sizes)):
+		linlist_times.append(time_search(linear_search,array_list[i],-1))
 	print("linlist times: ",linlist_times)
-
-	#zip
-	#res = zip(sizes,linlist_times,blist_times)
-	#res = [(val,time_search(linear_search(arraylist[val],-1)),time_search(binary_search(arraylist[val],-1))) for val in sizes]
-	res = [(val,linlist_times,blist_times) for val in sizes]
+	#res = [(val,time_search(linear_search,array_list[val],-1),time_search(binary_search,array_list[val],-1)) for val in sizes]
+	res = [(sizes[val],linlist_times[val],blist_times[val]) for val in range(len(sizes))]
 	return res;
 	###
 
